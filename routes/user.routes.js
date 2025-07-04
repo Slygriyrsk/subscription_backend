18cd0efbe3a8be88cc3fb342a7e37064fb86e64a
DevRizz <devruntimeerror69@gmail.com>
2025-07-04 23:05:37 +0530

    added authorization using tokens and _id

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,18 +1,23 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controllers/user.controller.js";
+import { authorize } from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => {
+userRouter.get("/", getUsers/* (req, res) => {
     res.send({
         title: "GET all Users"
     })
-})
+} */)
 
-userRouter.get("/:id", (req, res) => {
+//now here before moving to next that is getUSer we need a middleware to check if the user ID is valid or not
+//by passing a middleware function to the route
+//now we first need to sign-in to get the token and then use that token to access the user details
+userRouter.get("/:id", authorize, getUser/* (req, res) => {
     res.send({
         title: "GET User Details"
     })
-})
+} */)
 
 userRouter.post("/", (req, res) => {
     res.send({
